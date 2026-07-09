8d1d402f3f08d80b538b7cd31be10031e55bff09
y92hpst2t4-cyber <y92hpst2t4@privaterelay.appleid.com>
2026-07-09 13:37:48 -0400

    Create backgrounds.js

---
+++ backgrounds.js
@@ -0,0 +1,15 @@
+const backgrounds = {
+
+  arrival: {
+    colors: "linear-gradient(180deg,#70d8ff,#08657e)"
+  },
+
+  villa: {
+    colors: "linear-gradient(180deg,#58c7e8,#0b718c)"
+  },
+
+  crystal: {
+    colors: "linear-gradient(180deg,#1b335f,#061322)"
+  }
+
+};
