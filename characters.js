342dd25319652408ab6880b4aa99737e1ed80256
y92hpst2t4-cyber <y92hpst2t4@privaterelay.appleid.com>
2026-07-09 16:22:36 -0400

    Update characters.js

--- characters.js
+++ characters.js
@@ -7,10 +7,11 @@ const characters = {
   },
 
   host: {
-    name: "Host",
-    title: "Keeper of the Crystal Flame",
-    portrait: "🎭"
-  },
+    name: "The Host",
+    title: "Guardian of the Crystal Flame",
+    emoji: "🎭",
+    image: "images/F6AE1DF3-3320-4AD5-8141-6DD1929995FD.png"
+},
 
   lucas: {
     name: "Lucas Ramirez",
