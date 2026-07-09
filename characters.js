5c3984922becfa7cd1e5d924a8c472848fd23259
y92hpst2t4-cyber <y92hpst2t4@privaterelay.appleid.com>
2026-07-09 13:35:34 -0400

    Create characters.js

---
+++ characters.js
@@ -0,0 +1,27 @@
+const characters = {
+
+  narrator: {
+    name: "Narrator",
+    title: "The Story Begins",
+    portrait: "🌴"
+  },
+
+  host: {
+    name: "Host",
+    title: "Keeper of the Crystal Flame",
+    portrait: "🎭"
+  },
+
+  lucas: {
+    name: "Lucas Ramirez",
+    title: "The Loyal Swimmer",
+    portrait: "👦"
+  },
+
+  maya: {
+    name: "Maya Patel",
+    title: "The Explorer",
+    portrait: "🌿"
+  }
+
+};
