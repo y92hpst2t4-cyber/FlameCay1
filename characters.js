c759f7a76b43b36d972fc5fed0f7e8921d066fc8
y92hpst2t4-cyber <y92hpst2t4@privaterelay.appleid.com>
2026-07-09 20:01:37 -0400

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,9 +1,9 @@
 const characters = {
-
   narrator: {
     name: "Narrator",
     title: "The Story Begins",
-    portrait: "🌴"
+    emoji: "🌴",
+    image: ""
   },
 
   host: {
@@ -11,18 +11,61 @@ const characters = {
     title: "Guardian of the Crystal Flame",
     emoji: "🎭",
     image: "images/F6AE1DF3-3320-4AD5-8141-6DD1929995FD.png"
-},
+  },
 
   lucas: {
     name: "Lucas Ramirez",
     title: "The Loyal Swimmer",
-    portrait: "👦"
+    emoji: "👦",
+    image: "images/AF6CC476-58A0-42B0-8044-66A742190789.png"
   },
 
   maya: {
     name: "Maya Patel",
     title: "The Explorer",
-    portrait: "🌿"
-  }
+    emoji: "🌿",
+    image: "images/0487E1B8-561B-4FDC-BAAE-EE223F61D754.png"
+  },
 
+  kai: {
+    name: "Kai Nakamura",
+    title: "The Competitive Surfer",
+    emoji: "🌊",
+    image: "images/95C98E8C-8531-42AE-BAF7-DCB8A894E394.png"
+  },
+
+  isabella: {
+    name: "Isabella Cruz",
+    title: "The Social Butterfly",
+    emoji: "🦋",
+    image: "images/95F834E8-3ABC-4C2F-AF14-E8D121253968.png"
+  },
+
+  ethan: {
+    name: "Ethan Brooks",
+    title: "The CEO",
+    emoji: "💼",
+    image: "images/E607EF85-171E-4623-9D56-921695CE2F73.png"
+  },
+
+  sofia: {
+    name: "Sofia Rossi",
+    title: "The Heartbreaker",
+    emoji: "💖",
+    image: "images/286EA103-FC31-41AF-BD1E-3E55D7B2115F.png"
+  },
+
+  noah: {
+    name: "Noah Carter",
+    title: "The Firefighter",
+    emoji: "🚒",
+    image: "images/3C17246B-AD38-420C-8767-F53B7F3FBDDE.png"
+  },
+
+  lisa: {
+    name: "Lisa Morgan",
+    title: "The Bombshell",
+    emoji: "💖",
+    image: "images/9DBF1984-9450-4DAF-94D3-04E305D0E5DF.png"
+  }
 };
